54f023e30cd6cb3cb26fab851c2ddb88ec0df2a6
todanaoki-study <toda.naoki3131@gmail.com>
2025-03-16 22:08:53 +0900

    分からなすぎるんじゃパート２

--- portfolio-funcTest/scroll-test/js/index.js
+++ portfolio-funcTest/scroll-test/js/index.js
@@ -1,14 +1,6 @@
 const works = document.querySelector('.works');
 const container = document.querySelector('.works__container');
 
-//停止の関数
-// const scrollStop = (position) => {
-// }
-
 document.addEventListener('scroll', () => {
-    const containerPos = container.getBoundingClientRect();
 
-    if (containerPos.top <= 0) {
-        works.scrollTop = works.scrollTop - containerPos;
-    }
 });

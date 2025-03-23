d7a2f0b9078691dbffda0068ed38f2a64333d30e
todanaoki-study <toda.naoki3131@gmail.com>
2025-03-23 11:15:28 +0900

    繰り返しフィールド設定まで

--- microCMS-customField/js/index.js
+++ microCMS-customField/js/index.js
@@ -1,11 +1,11 @@
-const url = 0;
-const apiKey = 0;
+const url = "https://custom.microcms.io/api/v1/article";
+const apiKey = "E5M9ZT9l83wWVNFwxczqS2Ld0cgqgrKQuSIX";
 const fetchData = async () => {
     try {
         const response = await fetch(url,
             {
                 headers: {
-                    'X-API-KEY': 'apikeyを設定'
+                    'X-API-KEY': apiKey
                 }
             });
         const data = await response.json();
@@ -17,11 +17,21 @@ const fetchData = async () => {
 
 // html作成
 const renderPosts = (posts) => {
-    posts.forEach((item, index) => {
+    const container = document.querySelector('.blog__container');
+    container.innerHTML = '';
+    posts.forEach(item => {
         const cardHTML = `
-        `;
-        console.log(item.links);
-        document.querySelector('.articles__list').insertAdjacentHTML('beforeend', cardHTML);
+        <article class="blog__post post">
+        <img src="${item.image.url}" alt="" class="post__image">
+                    <h3 class="post__title">${item.title}</h3>
+                    <p class="post__author">${item.writer}</p>
+                    <p class="post__date">公開日: ${item.date}</p>
+                    <div class="post__content">
+                        <p>${item.overview}</p>
+                    </div>
+        </article>`+ `<p>あいうと</p>`;
+
+        container.insertAdjacentHTML('beforeend', cardHTML);
     });
 };
 

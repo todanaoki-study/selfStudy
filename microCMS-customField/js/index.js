6c3b1704c2775b6c2aa9fab539c72f89ffc16b4f
todanaoki-study <toda.naoki3131@gmail.com>
2025-03-23 12:12:10 +0900

    完成

--- microCMS-customField/js/index.js
+++ microCMS-customField/js/index.js
@@ -20,6 +20,18 @@ const renderPosts = (posts) => {
     const container = document.querySelector('.blog__container');
     container.innerHTML = '';
     posts.forEach(item => {
+        let snsHTML = "";
+        console.log(item.sns.length);
+        if (item.sns.length > 0) {
+            for (let i = 0; i < item.sns.length; i++) {
+                snsHTML += `
+                <div class="post__sns">
+                        <p class="post__snsTitle">${item.sns[i].sns}</p>
+                        <a class="post__snsLink" href="${item.sns[i].link}" target="_blank">${item.sns[i].link}</a>
+                    </div>
+                    `;
+            }
+        }
         const cardHTML = `
         <article class="blog__post post">
         <img src="${item.image.url}" alt="" class="post__image">
@@ -28,9 +40,10 @@ const renderPosts = (posts) => {
                     <p class="post__date">公開日: ${item.date}</p>
                     <div class="post__content">
                         <p>${item.overview}</p>
-                    </div>
-        </article>`+ `<p>あいうと</p>`;
+                        </div>
+                        ${snsHTML}
 
+        </article>`;
         container.insertAdjacentHTML('beforeend', cardHTML);
     });
 };
